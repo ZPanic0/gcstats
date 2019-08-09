c2229b664bdcadbc314c0984e773443fe1dc6f2d
James <zc.sgs5@gmail.com>
2019-08-09 16:59:36 -0400

    Import logic for new GetLastWeekTallyingPeriodId query from old project Add GetWeekTallyingPeriodIdsForCompletedScans query to help reduce retreading of old ground when getting and saving pages Add SaveAllMissingPages command to fetch and save any pages not already found in the database with optional callback to be executed during delay period

--- gcstats/Application.cs
+++ gcstats/Application.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using gcstats.Commands;
-using gcstats.Queries;
 using MediatR;
 
 namespace gcstats
 {
     public class Application
     {
+        private readonly IMediator mediator;
+
         public Application(IMediator mediator)
         {
-            Mediator = mediator;
+            this.mediator = mediator;
         }
-
-        public IMediator Mediator { get; }
 
         internal async Task Execute()
         {
-            await Mediator.Send(new SetupTables.Request());
+            await mediator.Send(new SetupTables.Request());
+            await mediator.Send(new SaveAllMissingPages.Request());
         }
     }
 }
---
+++ gcstats/Queries/GetLastWeekTallyingPeriodId.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace gcstats.Queries
+{
+    public abstract class GetLastWeekTallyingPeriodId
+    {
+        public class Request : IRequest<int> { }
+
+        public class Handler : IRequestHandler<Request, int>
+        {
+            public Task<int> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var now = DateTime.Now;
+                var lastCalculatedMonday = now.AddDays(-7 - (7 + (now.DayOfWeek - DayOfWeek.Monday)) % 7);
+                var weekNumber = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(
+                    lastCalculatedMonday,
+                    CalendarWeekRule.FirstFullWeek,
+                    DayOfWeek.Monday);
+
+                return Task.FromResult(lastCalculatedMonday.Year * 100 + weekNumber);
+            }
+        }
+    }
+}
