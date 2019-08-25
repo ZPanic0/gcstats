1b9154208bb084fa5206fa58e769844271949c37
James <zc.sgs5@gmail.com>
2019-08-25 06:29:28 -0400

    Discard complex multiple connection, multiple proxy behavior

--- gcstats/Modules/HttpClientModule.cs
+++ gcstats/Modules/HttpClientModule.cs
@@ -1,62 +1,19 @@
 ﻿using Autofac;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
 using System.Net.Http;
-using System.Linq;
+using System.Threading.Tasks;
 
 namespace gcstats.Modules
 {
     public class HttpClientModule : Module
     {
-        public static int ProxyCount => clientQueue.Count();
-        private static readonly Queue<HttpClient> clientQueue;
-
-        static HttpClientModule()
-        {
-            var proxyDetails = JsonConvert.DeserializeObject<IEnumerable<ProxyDetails>>(
-                File.ReadAllText($"{Directory.GetCurrentDirectory()}/proxydetails.json"));
-
-            clientQueue = new Queue<HttpClient>(proxyDetails
-                .Select(x => new HttpClient(
-                    new HttpClientHandler
-                    {
-                        Proxy = new WebProxy
-                        {
-                            Address = new Uri($"{x.Host}:{x.Port}"),
-                            BypassProxyOnLocal = false,
-                            UseDefaultCredentials = false,
-                            Credentials = new NetworkCredential(x.UserName, x.Password)
-                        }
-                    },
-                    true)).ToArray());
-        }
         protected override void Load(ContainerBuilder builder)
         {
-            builder
-                .Register(context =>
-                    {
-                        var client = clientQueue.Dequeue();
-
-                        while (client == null)
-                        {
-                            client = clientQueue.Dequeue();
-                        }
-
-                        clientQueue.Enqueue(client);
-                        return client;
-                    })
-                .ExternallyOwned();
+            builder.RegisterInstance(new HttpClient(new HttpClientHandler
+            {
+                MaxConnectionsPerServer = int.MaxValue
+                
+            }, true)).AsSelf().SingleInstance();
         }
-    }
-
-    public class ProxyDetails
-    {
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
     }
 }
