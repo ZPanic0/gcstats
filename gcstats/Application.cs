aa7b1bf8239948b3db80069db44f24076ee79df7
James <zc.sgs5@gmail.com>
2019-08-25 07:30:41 -0400

    Update Application with current callback structure

--- gcstats/Application.cs
+++ gcstats/Application.cs
@@ -1,7 +1,15 @@
 ﻿using gcstats.Commands;
+using gcstats.Commands.Database;
+using gcstats.Common;
+using gcstats.Modules;
 using gcstats.Queries;
 using MediatR;
+using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace gcstats
@@ -20,20 +28,8 @@ namespace gcstats
             await mediator.Send(new SetupTables.Request());
             await mediator.Send(new GetAllMissingPages.Request
             {
-                Callback = async (set) =>
-                {
-                    await mediator.Send(new SavePages.Request {
-                        Pages = set.Select(x => new SavePage.Request(
-                            x.Item1.TallyingPeriodId,
-                            x.Item1.TimePeriod,
-                            x.Item1.Server,
-                            x.Item1.Faction,
-                            x.Item1.Page,
-                            x.Item2))
-                    });
-                },
-                BatchSize = 18
-            });
+                Callback = (queue, getLockState) => mediator.Send(new SaveStreamedPageData.Request(getLockState, queue, 10000))
+            }); 
         }
     }
 }
