3fa07b51014f8c21872e6c6a7d42173cedcd0025
James <14856717+ZPanic0@users.noreply.github.com>
2019-09-01 02:37:22 -0400

    Move loading logic out of GetSavedPage and into new LoadFileToCache

---
+++ gcstats/Commands/LoadFileToCache.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
+using System.Threading.Tasks;
+using gcstats.Common;
+using gcstats.Configuration.Models;
+using MediatR;
+
+namespace gcstats.Commands
+{
+    public class LoadFileToCache
+    {
+        public class Request : IRequest {
+            public Request(int tallyingPeriodId)
+            {
+                TallyingPeriodId = tallyingPeriodId;
+            }
+
+            public int TallyingPeriodId { get; }
+        }
+
+        public class Handler : IRequestHandler<Request>
+        {
+            private readonly AppSettings appSettings;
+            private readonly PageCache pageCache;
+
+            public Handler(AppSettings appSettings, PageCache pageCache)
+            {
+                this.appSettings = appSettings;
+                this.pageCache = pageCache;
+            }
+            public async Task<Unit> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var path = string.Format(
+                    appSettings.ArchiveSettings.ArchivePathTemplate,
+                    appSettings.BaseDirectory,
+                    request.TallyingPeriodId);
+
+                using var file = File.OpenRead(path);
+                using var archive = new ZipArchive(file, ZipArchiveMode.Read);
+
+                foreach (var entry in archive.Entries)
+                {
+                    using var archiveEntry = new StreamReader(entry.Open());
+                    pageCache.Load(new Tuple<long, string>(long.Parse(entry.Name.Split('.')[0]), await archiveEntry.ReadToEndAsync()));
+                }
+
+                return Unit.Value;
+            }
+        }
+    }
+}
--- gcstats/Queries/GetSavedPage.cs
+++ gcstats/Queries/GetSavedPage.cs
@@ -1,9 +1,6 @@
 ﻿using gcstats.Common;
-using gcstats.Configuration.Models;
 using MediatR;
 using System;
-using System.IO;
-using System.IO.Compression;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,40 +20,21 @@ namespace gcstats.Queries
 
         public class Handler : IRequestHandler<Request, string>
         {
-            public Handler(AppSettings appSettings, PageCache pageCache)
+            public Handler(PageCache pageCache)
             {
-                this.appSettings = appSettings;
                 this.pageCache = pageCache;
             }
 
-            private readonly AppSettings appSettings;
             private readonly PageCache pageCache;
 
             public async Task<string> Handle(Request request, CancellationToken cancellationToken)
             {
                 if (!pageCache.Contains(request.IndexId))
                 {
-                    await LoadFile(request);
+                    throw new Exception($"IndexId not found in PageCache. Id: {request.IndexId}");
                 }
 
                 return pageCache.PopPage(request.IndexId);
-            }
-
-            private async Task LoadFile(Request request)
-            {
-                var path = string.Format(
-                    appSettings.ArchiveSettings.ArchivePathTemplate,
-                    appSettings.BaseDirectory,
-                    request.IndexId / 100000);
-
-                using var file = File.OpenRead(path);
-                using var archive = new ZipArchive(file, ZipArchiveMode.Read);
-
-                foreach (var entry in archive.Entries)
-                {
-                    using var archiveEntry = new StreamReader(entry.Open());
-                    pageCache.Load(new Tuple<long, string>(long.Parse(entry.Name.Split('.')[0]), await archiveEntry.ReadToEndAsync()));
-                }
             }
         }
     }
