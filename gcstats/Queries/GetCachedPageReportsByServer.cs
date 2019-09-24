f7f3588046a72775d3c88ad50456d341c312b353
James <14856717+ZPanic0@users.noreply.github.com>
2019-09-24 14:40:22 -0400

    Reimplement GetPageFromZip to operate directly on zip rather than trusting a cache Add GetCachedPageReportsByServer to fetch PageReports from a given server's bin file

---
+++ gcstats/Queries/GetCachedPageReportsByServer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using gcstats.Common.Enums;
+using gcstats.Common.ProtobufModels;
+using gcstats.Configuration.Models;
+using MediatR;
+using ProtoBuf;
+
+namespace gcstats.Queries
+{
+    public class GetCachedPageReportsByServer
+    {
+        public class Request : IRequest<IEnumerable<PageReport>> {
+            public Request(Server server)
+            {
+                Server = server;
+            }
+
+            public Server Server { get; }
+        }
+
+        public class Handler : IRequestHandler<Request, IEnumerable<PageReport>>
+        {
+            private readonly AppSettings appSettings;
+
+            public Handler(AppSettings appSettings)
+            {
+                this.appSettings = appSettings;
+            }
+
+            public Task<IEnumerable<PageReport>> Handle(Request request, CancellationToken cancellationToken)
+            {
+                using var fileStream = File.OpenRead(string.Format(
+                        appSettings.ProtobufSettings.CachePathTemplate,
+                        appSettings.BaseDirectory,
+                        request.Server));
+
+                return Task.FromResult(Serializer.Deserialize<IEnumerable<PageReport>>(fileStream));
+            }
+        }
+    }
+}
---
+++ gcstats/Queries/GetPageFromZip.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Threading;
+using System.Threading.Tasks;
+using gcstats.Configuration.Models;
+using MediatR;
+
+namespace gcstats.Queries
+{
+    public class GetPageFromZip
+    {
+        public class Request : IRequest<string>
+        {
+            public Request(long indexId)
+            {
+                IndexId = indexId;
+            }
+
+            public long IndexId { get;}
+        }
+
+        public class Handler : IRequestHandler<Request, string>
+        {
+            private readonly AppSettings appSettings;
+            private readonly IMediator mediator;
+
+            public Handler(AppSettings appSettings, IMediator mediator)
+            {
+                this.appSettings = appSettings;
+                this.mediator = mediator;
+            }
+
+            public async Task<string> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var data = await mediator.Send(new GetQueryDataFromIndex.Request(request.IndexId));
+
+                using var fileStream = File.OpenRead(string.Format(
+                        appSettings.ArchiveSettings.ArchivePathTemplate,
+                        appSettings.BaseDirectory,
+                        data.TallyingPeriodId));
+
+                using var archive = new ZipArchive(fileStream, ZipArchiveMode.Read);
+
+                var archiveEntry = archive.GetEntry(
+                    string.Format(
+                        appSettings.ArchiveSettings.FileNameTemplate, 
+                        request.IndexId));
+
+                using var reader = new StreamReader(archiveEntry.Open());
+
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
