fe2de2962f8a4997fed5c21543a546b68b731d1b
James <14856717+ZPanic0@users.noreply.github.com>
2019-10-26 12:17:31 -0400

    Add PageMenu component to ease navigating paginated content when determining what to display only requires the page number Add Clamp utility function as project does not currently support Math.clamp

---
+++ src/components/Common/PageMenu.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react"
+import { Menu, Icon } from "semantic-ui-react"
+import Clamp from "../../utilities/Clamp"
+
+export default class PageMenu extends Component {
+    constructor(props) {
+        super(props)
+
+        this.handleOnClick = this.handleOnClick.bind(this)
+    }
+
+    getPageMenuItems() {
+        const items = [
+            <Menu.Item
+                key="leftChevron"
+                disabled={this.props.selectedPage < this.props.pages - 2}
+                as='a'
+                icon
+                value={-5}
+                onClick={this.handleOnClick}
+            >
+                <Icon name='chevron left' />
+            </Menu.Item>
+        ]
+
+        for (let buttonValue = -2; buttonValue < 3; buttonValue++) {
+            const pageValue = this.props.selectedPage + buttonValue
+
+            if (pageValue > 0 && pageValue <= this.props.pages) {
+                items.push(
+                    <Menu.Item
+                        key={buttonValue}
+                        as="a"
+                        value={buttonValue}
+                        active={buttonValue === 0}
+                        onClick={this.handleOnClick}
+                    >
+                        {this.props.selectedPage + buttonValue}
+                    </Menu.Item>
+                )
+            }
+        }
+
+        items.push(
+            <Menu.Item
+                key="rightChevron"
+                disabled={this.props.selectedPage > 3}
+                as='a'
+                icon
+                value={5}
+                onClick={this.handleOnClick}
+            >
+                <Icon name='chevron right' />
+            </Menu.Item>
+        )
+
+        return items
+    }
+
+    handleOnClick(e, { value }) {
+        this.props.onSelect(Clamp(this.props.selectedPage + value, 1, this.props.pages))
+    }
+
+    render() {
+        return (
+            <Menu floated={this.props.floated} pagination>
+                {this.getPageMenuItems()}
+            </Menu>
+        )
+    }
+}
