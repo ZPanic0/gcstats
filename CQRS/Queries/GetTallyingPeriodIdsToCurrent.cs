85cb51808d38c42412421ace429c00ee1c3a928e
ZPanic0 <14856717+ZPanic0@users.noreply.github.com>
2020-04-10 12:58:41 -0400

    Move GetTallyingPeriodIdsToCurrent to CQRS project
    Add moq
    Add tests

---
+++ CQRS/Queries/GetTallyingPeriodIdsToCurrent.cs
@@ -0,0 +1,56 @@
+﻿using gcstats.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRS.Queries
+{
+    public static class GetTallyingPeriodIdsToCurrent
+    {
+        public class Request : IRequest<IEnumerable<int>> {
+            public Request(DateTime now)
+            {
+                Now = now;
+            }
+
+            public DateTime Now { get; }
+        }
+
+        public class Handler : IRequestHandler<Request, IEnumerable<int>>
+        {
+            private readonly IMediator mediator;
+
+            public Handler(IMediator mediator)
+            {
+                this.mediator = mediator;
+            }
+
+            public async Task<IEnumerable<int>> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var tallyingPeriodIds = Enumerable.Empty<int>();
+                var lastTallyingPeriodId = await mediator.Send(new GetLastWeekTallyingPeriodId.Request(request.Now));
+                var lastTallyingPeriodYear = lastTallyingPeriodId / 100;
+
+                for (var year = 2014; year < lastTallyingPeriodYear; year++)
+                {
+                    var weeksInYear = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(
+                        new DateTime(year, 12, 31),
+                        CalendarWeekRule.FirstFullWeek,
+                        DayOfWeek.Monday);
+                    var startOfYear = year * 100 + 1;
+                    tallyingPeriodIds = tallyingPeriodIds
+                        .Concat(Enumerable.Range(startOfYear, weeksInYear));
+                }
+
+                tallyingPeriodIds = tallyingPeriodIds
+                    .Concat(Enumerable.Range(lastTallyingPeriodYear * 100 + 1, lastTallyingPeriodId % (lastTallyingPeriodYear * 100)));
+
+                return tallyingPeriodIds;
+            }
+        }
+    }
+}
---
+++ Tests.Unit/Queries/GetTallyingPeriodIdsToCurrentTests.cs
@@ -0,0 +1,44 @@
+﻿using CQRS.Queries;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Unit.Queries
+{
+    public class GetTallyingPeriodIdsToCurrentTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public async Task ReturnsExpectedIds(int latestId, IEnumerable<int> expectedIds)
+        {
+            var mediatorMock = new Mock<IMediator>();
+            mediatorMock
+                .Setup(x => x.Send(It.IsAny<IRequest<int>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(latestId);
+            (await new GetTallyingPeriodIdsToCurrent.Handler(mediatorMock.Object)
+                .Handle(new GetTallyingPeriodIdsToCurrent.Request(DateTime.Now), CancellationToken.None))
+                .Should()
+                .BeEquivalentTo(expectedIds);
+        }
+
+        public static IEnumerable<object[]> TestData => new object[][] {
+            //Exercising unique week counting logic of the lodestone
+            new object[] {
+                202013,
+                Enumerable.Range(1, 52).Select(week => 201400 + week)
+                .Concat(Enumerable.Range(1, 52).Select(week => 201500 + week))
+                .Concat(Enumerable.Range(1, 52).Select(week => 201600 + week))
+                .Concat(Enumerable.Range(1, 52).Select(week => 201700 + week))
+                .Concat(Enumerable.Range(1, 53).Select(week => 201800 + week))
+                .Concat(Enumerable.Range(1, 52).Select(week => 201900 + week))
+                .Concat(Enumerable.Range(1, 13).Select(week => 202000 + week))
+            }
+        };
+    }
+}
