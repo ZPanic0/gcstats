27e0dd2d5a4949faa7fbd07ec1286d80379e2d8c
ZPanic0 <14856717+ZPanic0@users.noreply.github.com>
2020-04-10 16:40:04 -0400

    Import Sets caching object
    Add tests for Sets

---
+++ Common/Sets.cs
@@ -0,0 +1,36 @@
+﻿using Common.Enums;
+using Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CQRS.Common
+{
+    public class Sets
+    {
+        public Sets()
+        {
+            Datacenters = Array.AsReadOnly(((Datacenter[])Enum.GetValues(typeof(Datacenter))).Skip(1).ToArray());
+            Servers = BuildServers();
+            Factions = Array.AsReadOnly(((Faction[])Enum.GetValues(typeof(Faction))).Skip(1).ToArray());
+            PageNumbers = Array.AsReadOnly(Enumerable.Range(1, 5).ToArray());
+        }
+
+        public IEnumerable<Datacenter> Datacenters { get; }
+        public (ReadOnlyDictionary<Datacenter, ReadOnlyCollection<Server>> Dictionary, ReadOnlyCollection<Server> All) Servers { get; }
+        public IEnumerable<Faction> Factions { get; }
+        public IEnumerable<int> PageNumbers { get; }
+
+        private (ReadOnlyDictionary<Datacenter, ReadOnlyCollection<Server>> Dictionary, ReadOnlyCollection<Server> All) BuildServers()
+        {
+            var dictionary = Datacenters.ToDictionary(
+                datacenter => datacenter, 
+                datacenter => new ReadOnlyCollection<Server>(datacenter.GetServers().ToArray()));
+
+            return (
+                Dictionary: new ReadOnlyDictionary<Datacenter, ReadOnlyCollection<Server>>(dictionary), 
+                All: Array.AsReadOnly(dictionary.Values.SelectMany(x => x).ToArray()));
+        }
+    }
+}
---
+++ Tests.Unit/SetsTests.cs
@@ -0,0 +1,96 @@
+﻿using Common.Enums;
+using Common.Extensions;
+using CQRS.Common;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Tests.Unit
+{
+    public class SetsTests
+    {
+        [Fact]
+        public void EmitsAllDatacenters()
+        {
+            var result = new Sets().Datacenters;
+            var datacenterValues = (Datacenter[])Enum.GetValues(typeof(Datacenter));
+
+            result
+                .Should()
+                .HaveCount(datacenterValues.Length - 1);
+
+            datacenterValues
+                .Except(result)
+                .Should()
+                .BeEquivalentTo(new[] { Datacenter.NoInput });
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void EmitsExpectedServers(Datacenter datacenter, IEnumerable<Server> server)
+        {
+            new Sets().Servers.Dictionary[datacenter]
+                .Should()
+                .BeEquivalentTo(server);
+        }
+
+        public static IEnumerable<object[]> TestData() => new object[][] {
+            new object[] { Datacenter.Elemental, Datacenter.Elemental.GetServers() },
+            new object[] { Datacenter.Gaia, Datacenter.Gaia.GetServers() },
+            new object[] { Datacenter.Mana, Datacenter.Mana.GetServers() },
+            new object[] { Datacenter.Aether, Datacenter.Aether.GetServers() },
+            new object[] { Datacenter.Primal, Datacenter.Primal.GetServers() },
+            new object[] { Datacenter.Crystal, Datacenter.Crystal.GetServers() },
+            new object[] { Datacenter.Chaos, Datacenter.Chaos.GetServers() },
+            new object[] { Datacenter.Light, Datacenter.Light.GetServers() }
+        };
+
+        [Fact]
+        public void EmitsAllServers()
+        {
+            var result = new Sets().Servers.All;
+            var serverValues = (Server[])Enum.GetValues(typeof(Server));
+
+            result
+                .Should()
+                .HaveCount(serverValues.Length - 1);
+
+            serverValues
+                .Except(result)
+                .Should()
+                .BeEquivalentTo(new[] { Datacenter.NoInput });
+        }
+
+        [Fact]
+        public void EmitsAllFactions()
+        {
+            var result = new Sets().Factions;
+            var factionValues = (Faction[])Enum.GetValues(typeof(Faction));
+
+            result
+                .Should()
+                .HaveCount(factionValues.Length - 1);
+
+            factionValues
+                .Except(result)
+                .Should()
+                .BeEquivalentTo(new[] { Datacenter.NoInput });
+        }
+
+        [Fact]
+        public void EmitsExpectedPageCounts()
+        {
+            var result = new Sets().PageNumbers;
+
+            result
+                .Should()
+                .HaveCount(5);
+
+            result
+                .Should()
+                .BeEquivalentTo(new[] { 1, 2, 3, 4, 5 });
+        }
+    }
+}
