cad70e6d091aaa86122d56925d414ef10f5e8239
ZPanic0 <14856717+ZPanic0@users.noreply.github.com>
2020-04-10 16:01:10 -0400

    Import ServerExtensions
    Add tests covering ServerExtensions

---
+++ Common/Extensions/ServerExtensions.cs
@@ -0,0 +1,95 @@
+﻿using Common.Enums;
+using System;
+
+namespace Common.Extensions
+{
+    public static class ServerExtensions
+    {
+        public static Datacenter GetDatacenter(this Server server)
+        {
+            switch (server)
+            {
+                case Server.Aegis:
+                case Server.Atomos:
+                case Server.Carbuncle:
+                case Server.Garuda:
+                case Server.Gungnir:
+                case Server.Kujata:
+                case Server.Ramuh:
+                case Server.Tonberry:
+                case Server.Typhon:
+                case Server.Unicorn:
+                    return Datacenter.Elemental;
+                case Server.Alexander:
+                case Server.Bahamut:
+                case Server.Durandal:
+                case Server.Fenrir:
+                case Server.Ifrit:
+                case Server.Ridill:
+                case Server.Tiamat:
+                case Server.Ultima:
+                case Server.Valefor:
+                case Server.Yojimbo:
+                case Server.Zeromus:
+                    return Datacenter.Gaia;
+                case Server.Anima:
+                case Server.Asura:
+                case Server.Belias:
+                case Server.Chocobo:
+                case Server.Hades:
+                case Server.Ixion:
+                case Server.Mandragora:
+                case Server.Masamune:
+                case Server.Pandaemonium:
+                case Server.Shinryu:
+                case Server.Titan:
+                    return Datacenter.Mana;
+                case Server.Adamantoise:
+                case Server.Cactuar:
+                case Server.Faerie:
+                case Server.Gilgamesh:
+                case Server.Jenova:
+                case Server.Midgardsormr:
+                case Server.Sargatanas:
+                case Server.Siren:
+                    return Datacenter.Aether;
+                case Server.Behemoth:
+                case Server.Excalibur:
+                case Server.Exodus:
+                case Server.Famfrit:
+                case Server.Hyperion:
+                case Server.Lamia:
+                case Server.Leviathan:
+                case Server.Ultros:
+                    return Datacenter.Primal;
+                case Server.Balmung:
+                case Server.Brynhildr:
+                case Server.Coeurl:
+                case Server.Diabolos:
+                case Server.Goblin:
+                case Server.Malboro:
+                case Server.Mateus:
+                case Server.Zalera:
+                    return Datacenter.Crystal;
+                case Server.Cerberus:
+                case Server.Louisoix:
+                case Server.Moogle:
+                case Server.Omega:
+                case Server.Ragnarok:
+                case Server.Spriggan:
+                    return Datacenter.Chaos;
+                case Server.Lich:
+                case Server.Odin:
+                case Server.Phoenix:
+                case Server.Shiva:
+                case Server.Twintania:
+                case Server.Zodiark:
+                    return Datacenter.Light;
+                case Server.NoInput:
+                    throw new ArgumentException("No server provided.");
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Tests.Unit/Extensions/ServerExtensionsTests.cs
@@ -0,0 +1,117 @@
+﻿using Common.Enums;
+using Common.Extensions;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests.Unit.Extensions
+{
+    public class ServerExtensionsTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void EmitsExpectedDatacenter(Server server, Datacenter expectedDatacenter)
+        {
+            server
+                .GetDatacenter()
+                .Should()
+                .Be(expectedDatacenter);
+        }
+
+        [Fact]
+        public void ThrowsOnNoInput()
+        {
+            Server.NoInput
+                .Invoking(server => server.GetDatacenter())
+                .Should().Throw<ArgumentException>();
+        }
+
+        public static IEnumerable<object[]> TestData() => new object[][]
+        {
+            //Elemental
+            new object[] { Server.Aegis, Datacenter.Elemental },
+            new object[] { Server.Atomos, Datacenter.Elemental },
+            new object[] { Server.Carbuncle, Datacenter.Elemental },
+            new object[] { Server.Garuda, Datacenter.Elemental },
+            new object[] { Server.Gungnir, Datacenter.Elemental },
+            new object[] { Server.Kujata, Datacenter.Elemental },
+            new object[] { Server.Ramuh, Datacenter.Elemental },
+            new object[] { Server.Tonberry, Datacenter.Elemental },
+            new object[] { Server.Typhon, Datacenter.Elemental },
+            new object[] { Server.Unicorn, Datacenter.Elemental },
+
+            //Gaia
+            new object[] { Server.Alexander, Datacenter.Gaia },
+            new object[] { Server.Bahamut, Datacenter.Gaia },
+            new object[] { Server.Durandal, Datacenter.Gaia },
+            new object[] { Server.Fenrir, Datacenter.Gaia },
+            new object[] { Server.Ifrit, Datacenter.Gaia },
+            new object[] { Server.Ridill, Datacenter.Gaia },
+            new object[] { Server.Tiamat, Datacenter.Gaia },
+            new object[] { Server.Ultima, Datacenter.Gaia },
+            new object[] { Server.Valefor, Datacenter.Gaia },
+            new object[] { Server.Yojimbo, Datacenter.Gaia },
+            new object[] { Server.Zeromus, Datacenter.Gaia },
+
+            //Mana
+            new object[] { Server.Anima, Datacenter.Mana },
+            new object[] { Server.Asura, Datacenter.Mana },
+            new object[] { Server.Belias, Datacenter.Mana },
+            new object[] { Server.Chocobo, Datacenter.Mana },
+            new object[] { Server.Hades, Datacenter.Mana },
+            new object[] { Server.Ixion, Datacenter.Mana },
+            new object[] { Server.Mandragora, Datacenter.Mana },
+            new object[] { Server.Masamune, Datacenter.Mana },
+            new object[] { Server.Pandaemonium, Datacenter.Mana },
+            new object[] { Server.Shinryu, Datacenter.Mana },
+            new object[] { Server.Titan, Datacenter.Mana },
+
+            //Aether
+            new object[] { Server.Adamantoise, Datacenter.Aether },
+            new object[] { Server.Cactuar, Datacenter.Aether },
+            new object[] { Server.Faerie, Datacenter.Aether },
+            new object[] { Server.Gilgamesh, Datacenter.Aether },
+            new object[] { Server.Jenova, Datacenter.Aether },
+            new object[] { Server.Midgardsormr, Datacenter.Aether },
+            new object[] { Server.Sargatanas, Datacenter.Aether },
+            new object[] { Server.Siren, Datacenter.Aether },
+
+            //Primal
+            new object[] { Server.Behemoth, Datacenter.Primal },
+            new object[] { Server.Excalibur, Datacenter.Primal },
+            new object[] { Server.Exodus, Datacenter.Primal },
+            new object[] { Server.Famfrit, Datacenter.Primal },
+            new object[] { Server.Hyperion, Datacenter.Primal },
+            new object[] { Server.Lamia, Datacenter.Primal },
+            new object[] { Server.Leviathan, Datacenter.Primal },
+            new object[] { Server.Ultros, Datacenter.Primal },
+
+            //Crystal
+            new object[] { Server.Balmung, Datacenter.Crystal },
+            new object[] { Server.Brynhildr, Datacenter.Crystal },
+            new object[] { Server.Coeurl, Datacenter.Crystal },
+            new object[] { Server.Diabolos, Datacenter.Crystal },
+            new object[] { Server.Goblin, Datacenter.Crystal },
+            new object[] { Server.Malboro, Datacenter.Crystal },
+            new object[] { Server.Mateus, Datacenter.Crystal },
+            new object[] { Server.Zalera, Datacenter.Crystal },
+
+            //Chaos
+            new object[] { Server.Cerberus, Datacenter.Chaos },
+            new object[] { Server.Louisoix, Datacenter.Chaos },
+            new object[] { Server.Moogle, Datacenter.Chaos },
+            new object[] { Server.Omega, Datacenter.Chaos },
+            new object[] { Server.Ragnarok, Datacenter.Chaos },
+            new object[] { Server.Spriggan, Datacenter.Chaos },
+
+            //Light
+            new object[] { Server.Lich, Datacenter.Light },
+            new object[] { Server.Odin, Datacenter.Light },
+            new object[] { Server.Phoenix, Datacenter.Light },
+            new object[] { Server.Shiva, Datacenter.Light },
+            new object[] { Server.Twintania, Datacenter.Light },
+            new object[] { Server.Zodiark, Datacenter.Light }
+        };
+    }
+}
